6e9c158d8724889641253313c6bb850639a5b61e
m74als <m74als@gmail.com>
2022-10-10 19:51:43 +0500

    10.10-2

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<<!doctype html>
+<!doctype html>
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
@@ -17,5 +17,33 @@
             ?>
         </select>
     </form>
+
+    <form action="" method="post" name="two">
+        <p>
+            Логин <input name="login" type="text">
+        </p>
+        <p>
+            Пароль <input name="password" type="password">
+        </p>
+        <button type="submit">
+            Войти!
+        </button>
+    </form>
+
+    <?php
+    print_r($_POST);
+    $arr_passwd = [
+        "Виктор" => "698d51a19d8a121ce581499d7b701668", // md5-хэш для пароля 111
+        "Регина" => "bcbe3365e6ac95ea2c0343a2395834dd", // md5-хэш для пароля 222
+    ];
+    if ($_POST) {
+        if ($arr_passwd[$_POST["login"]] == md5($_POST["password"])) {
+            echo "<p> Добро пожаловать, " . $_POST["login"] . "!<br></p>";
+        }
+        else {
+            echo "<p>  Нет такого пользователя!<br></p>";
+        }
+    }
+    ?>
 </body>
 </html>
