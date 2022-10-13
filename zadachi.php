3057cb3fc0bec1ae22d6825d1868578edbae12cb
Nikiforov Nikita <2naikn@gmail.com>
2022-10-13 19:27:16 +0300

    1310

---
+++ zadachi.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="">
+    <input type="text" name="login">
+    <input type="submit">
+    <p>
+        <?php
+        if($_SESSION['time']){
+            $_SESSION['time'] = date('Y-m-d H:i:s');
+        }
+        echo date('Y-m-d H:i:s');
+        ?>
+    </p>
+</form>
+</body>
+</html>
